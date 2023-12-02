ca8dde06a7ab3ea71991620df7c19305db11c0ad
shitakemura <shitakemura@gmail.com>
2023-12-03 07:55:37 +0900

    ~ 1.2

--- elm-files/src/Main.elm
+++ elm-files/src/Main.elm
@@ -3,5 +3,35 @@ module Main exposing (main)
 import Html exposing (text)
 
 
+greeting : String
+greeting =
+    "Hello, Static Elm!"
+
+
+meaningOfLife : Int
+meaningOfLife =
+    42
+
+
+pi : Float
+pi =
+    3.14
+
+
+canChuck : Bool
+canChuck =
+    True
+
+
+sayHello : String -> String
+sayHello name =
+    "Hello, " ++ name ++ "."
+
+
+bottlesOf : String -> Int -> String
+bottlesOf contents amount =
+    String.fromInt amount ++ " bottles of " ++ contents ++ " on the wall."
+
+
 main =
-    text "Hello, Elm!"
+    text (bottlesOf "juice" 99)
