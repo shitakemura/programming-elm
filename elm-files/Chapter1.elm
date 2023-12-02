db9128f2a883b716b52a2cc1a7751f47646c9188
shitakemura <shitakemura@gmail.com>
2023-12-02 15:24:00 +0900

    ~ 1.1

--- elm-files/Chapter1.elm
+++ elm-files/Chapter1.elm
@@ -19,3 +19,21 @@ tribllesStatus howMany =
         "They're consuming our supplies and returning nothing."
     else
         "I gave 'em to the Kiligons, sir."
+
+-- プログラムを組み立てる部品として関数を使う
+person name other = sayHello "Hi" other ++ " My name is " ++ name ++ "."
+
+person name greet other = greet other ++ " My name is " ++ name ++ "."
+
+-- person "Jeremy" (\other -> sayHello "Hi" other) "Tucker"
+person "Jeremy" (\other -> other ++ ", how are you?") "Tucker"
+
+-- 引数を部分適用する
+
+-- person "Jeremy" (sayHello "Hi") "Tucker"
+
+-- jeremy = person "Jeremy" (sayHello "Hi")
+-- jeremy "Tucker"
+
+-- tucker = person "Tucker" (\other -> other ++ ", how are you?")
+-- tucker "Jeremy"
