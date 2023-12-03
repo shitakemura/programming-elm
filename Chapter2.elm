7bd5e84e59a5126ee63b5a016faf3eae46b958e8
shitakemura <shitakemura@gmail.com>
2023-12-03 19:52:24 +0900

    ~ p42

--- Chapter2.elm
+++ Chapter2.elm
@@ -6,3 +6,13 @@
 
 -- レコード型を使ってみる
 dog = { name = "Tucker", age = 11 }
+
+-- 新しいレコードを作成する
+
+haveBirthday d = { name = d.name, age = d.age + 1 }
+
+olderDog = haveBirthday dog 
+
+-- レコード更新構文を使う
+
+haveBirthday d = { d | age = d.age + 1 }
