ecf130f840cd4c6842e938dc85a0850b46d26e7d
shitakemura <shitakemura@gmail.com>
2023-12-03 13:40:17 +0900

    ~ p30

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 - cd elm-files
 - elm init
 - elm make src/Make.elm
+
+- elm make src/Picshare.elm --output picshare.js
--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,1 +1,6 @@
 module Picshare exposing (main)
+
+import Html exposing (Html, div, text)
+
+main : Html msg 
+main = div [] [text "Picshare"]
