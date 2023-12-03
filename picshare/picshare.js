175fc65a97bc4a2aa04ff38ac79bce427dd15589
shitakemura <shitakemura@gmail.com>
2023-12-03 13:56:48 +0900

    ~ p32

--- picshare/picshare.js
+++ picshare/picshare.js
@@ -4419,7 +4419,25 @@ var $elm$virtual_dom$VirtualDom$toHandlerInt = function (handler) {
 			return 3;
 	}
 };
+var $elm$json$Json$Encode$string = _Json_wrap;
+var $elm$html$Html$Attributes$stringProperty = F2(
+	function (key, string) {
+		return A2(
+			_VirtualDom_property,
+			key,
+			$elm$json$Json$Encode$string(string));
+	});
+var $elm$html$Html$Attributes$class = $elm$html$Html$Attributes$stringProperty('className');
 var $elm$html$Html$div = _VirtualDom_node('div');
+var $elm$html$Html$h1 = _VirtualDom_node('h1');
+var $elm$html$Html$h2 = _VirtualDom_node('h2');
+var $elm$html$Html$img = _VirtualDom_node('img');
+var $elm$html$Html$Attributes$src = function (url) {
+	return A2(
+		$elm$html$Html$Attributes$stringProperty,
+		'src',
+		_VirtualDom_noJavaScriptOrHtmlUri(url));
+};
 var $elm$virtual_dom$VirtualDom$text = _VirtualDom_text;
 var $elm$html$Html$text = $elm$virtual_dom$VirtualDom$text;
 var $author$project$Picshare$main = A2(
@@ -4427,6 +4445,65 @@ var $author$project$Picshare$main = A2(
 	_List_Nil,
 	_List_fromArray(
 		[
-			$elm$html$Html$text('Picshare')
+			A2(
+			$elm$html$Html$div,
+			_List_fromArray(
+				[
+					$elm$html$Html$Attributes$class('header')
+				]),
+			_List_fromArray(
+				[
+					A2(
+					$elm$html$Html$h1,
+					_List_Nil,
+					_List_fromArray(
+						[
+							$elm$html$Html$text('Picshare')
+						]))
+				])),
+			A2(
+			$elm$html$Html$div,
+			_List_fromArray(
+				[
+					$elm$html$Html$Attributes$class('content-flow')
+				]),
+			_List_fromArray(
+				[
+					A2(
+					$elm$html$Html$div,
+					_List_fromArray(
+						[
+							$elm$html$Html$Attributes$class('detailed-photo')
+						]),
+					_List_fromArray(
+						[
+							A2(
+							$elm$html$Html$img,
+							_List_fromArray(
+								[
+									$elm$html$Html$Attributes$src('https://programming-elm.com/1.jpg')
+								]),
+							_List_Nil),
+							A2(
+							$elm$html$Html$div,
+							_List_fromArray(
+								[
+									$elm$html$Html$Attributes$class('photo-info')
+								]),
+							_List_fromArray(
+								[
+									A2(
+									$elm$html$Html$h2,
+									_List_fromArray(
+										[
+											$elm$html$Html$Attributes$class('caption')
+										]),
+									_List_fromArray(
+										[
+											$elm$html$Html$text('Surfing')
+										]))
+								]))
+						]))
+				]))
 		]));
 _Platform_export({'Picshare':{'init':_VirtualDom_init($author$project$Picshare$main)(0)(0)}});}(this));
--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,6 +1,18 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
 
 main : Html msg 
-main = div [] [text "Picshare"]
+main = 
+    div []
+    [ div [ class "header" ] 
+            [ h1 [] [ text "Picshare" ] ]
+    , div [ class "content-flow" ]
+        [ div [ class "detailed-photo" ]
+            [ img [ src "https://programming-elm.com/1.jpg" ] []
+            , div [ class "photo-info" ]
+                [ h2 [ class "caption" ] [ text "Surfing" ] ]
+            ]
+        ]
+    ]
