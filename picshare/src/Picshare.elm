bf697182ab4667ed8f77c1dedd222a4e770e1f20
shitakemura <shitakemura@gmail.com>
2023-12-03 22:27:32 +0900

    ~ p51

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -2,15 +2,17 @@ module Picshare exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 baseUrl : String
 baseUrl =
     "https://programming-elm.com/"
 
-initialModel : { url : String, caption : String }
+initialModel : { url : String, caption : String, liked : Bool }
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfing"
+    , liked = False
     }
 
 -- viewDetailedPhoto : String -> String -> Html msg 
@@ -33,15 +35,36 @@ initialModel =
 --         ]
 --     ]
 
-viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
 viewDetailedPhoto model =
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+            else
+                "fa-heart-o"
+        msg =
+            if model.liked then
+                Unlike
+            else
+                like
+    in
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text model.caption ] ]
+            [ div [ class "like-button" ]
+                [ i 
+                    [ class "fa fa-2x" 
+                    , class buttonClass
+                    , onClick msg
+                    ]
+                    []
+                ]
+            , [ h2 [ class "caption" ] [ text model.caption ] ]
+            ]
         ]
 
-view : { url : String, caption : String } -> Html msg
+view : { url : String, caption : String, liked : Bool } -> Html Msg
 view model =
     div []
         [ div [ class "header" ]
