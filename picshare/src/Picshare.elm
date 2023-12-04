7b8ace47ed6c348a93f81846fa3a63811993fb3a
shitakemura <shitakemura@gmail.com>
2023-12-04 21:13:09 +0900

    ~ p58

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,12 +1,15 @@
 module Picshare exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 
+
 baseUrl : String
 baseUrl =
     "https://programming-elm.com/"
+
 
 initialModel : { url : String, caption : String, liked : Bool }
 initialModel =
@@ -15,18 +18,19 @@ initialModel =
     , liked = False
     }
 
--- viewDetailedPhoto : String -> String -> Html msg 
+
+
+-- viewDetailedPhoto : String -> String -> Html msg
 -- viewDetailedPhoto url caption =
 --     div [ class "detailed-photo" ]
 --         [ img [ src url ] []
 --         , div [ class "photo-info" ]
 --             [ h2 [ class "caption" ] [ text caption ] ]
 --         ]
-
--- main : Html msg 
--- main = 
+-- main : Html msg
+-- main =
 --     div []
---     [ div [ class "header" ] 
+--     [ div [ class "header" ]
 --             [ h1 [] [ text "Picshare" ] ]
 --     , div [ class "content-flow" ]
 --         [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
@@ -35,17 +39,21 @@ initialModel =
 --         ]
 --     ]
 
+
 viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
 viewDetailedPhoto model =
     let
         buttonClass =
             if model.liked then
                 "fa-heart"
+
             else
                 "fa-heart-o"
+
         msg =
             if model.liked then
                 Unlike
+
             else
                 Like
     in
@@ -53,8 +61,8 @@ viewDetailedPhoto model =
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
             [ div [ class "like-button" ]
-                [ i 
-                    [ class "fa fa-2x" 
+                [ i
+                    [ class "fa fa-2x"
                     , class buttonClass
                     , onClick msg
                     ]
@@ -63,6 +71,7 @@ viewDetailedPhoto model =
             , h2 [ class "caption" ] [ text model.caption ]
             ]
         ]
+
 
 view : { url : String, caption : String, liked : Bool } -> Html Msg
 view model =
@@ -73,9 +82,11 @@ view model =
             [ viewDetailedPhoto model ]
         ]
 
+
 type Msg
     = Like
     | Unlike
+
 
 update :
     Msg
@@ -85,9 +96,21 @@ update msg model =
     case msg of
         Like ->
             { model | liked = True }
+
         Unlike ->
             { model | liked = False }
 
-main : Html Msg
+
+
+-- main : Html Msg
+-- main =
+--     view initialModel
+
+
+main : Program () { url : String, caption : String, liked : Bool } Msg
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
