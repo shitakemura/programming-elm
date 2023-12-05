c4d462d73d43a7d7ae011d9810d1f1c302cab323
shitakemura <shitakemura@gmail.com>
2023-12-06 08:17:21 +0900

    ~ p90

--- Chapter4.elm
+++ Chapter4.elm
@@ -5,3 +5,22 @@
 -- 問題を理解する
 
 -- 最初にやること
+
+-- デコーダーで遊んでみよう
+
+import Json.Decode exposing (decodeString, bool, int, string)
+
+decodeString int "42"
+decodeString int "\"Elm\""
+
+type Result error value
+    = Ok value
+    | Err error
+
+-- 
+import Json.Decode exposing (decodeString, bool, field, int, list, string)
+
+decodeString bool "true"
+decodeString string "\"Elm is Awesome\""
+decodeString (list int) "[1, 2, 3]"
+decodeString (field "name" string) """{"name": "Tucker"}"""
